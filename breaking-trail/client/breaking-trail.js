8e151dda0b5ea69e22b45741caad3f020d97d7d4
Justin Antinarella <jantinarella@backcountry.com>
2014-12-15 15:11:37 -0700

    Working routes

--- breaking-trail/client/breaking-trail.js
+++ breaking-trail/client/breaking-trail.js
@@ -26,3 +26,14 @@ Template.activity.events({
         return false;
     }
 });
+
+// Router.route('/homexxx', function () {
+//   // render the Home template with a custom data context
+//   this.render('home', {data: {title: 'My Title'}});
+// });
+
+// // when you navigate to "/one" automatically render the template named "One".
+// Router.route('/one');
+
+// // when you navigate to "/two" automatically render the template named "Two".
+// Router.route('/two');
--- breaking-trail/routes.js
+++ breaking-trail/routes.js
@@ -1,24 +1,36 @@
 Router.route('/', function () {
-  this.render('home');
+  // render the Home template with a custom data context
+  this.render('Home', {data: {title: 'My Title'}});
 });
 
-Router.route('/activity', function () {
-  this.render('activity');
-});
+// when you navigate to "/home" automatically render the template named "home".
+Router.route('/home');
+Router.route('/one');
+Router.route('/activity');
+Router.route('/activities');
 
-Router.route('/items/:_id', function () {
-  var item = Items.findOne({_id: this.params._id});
-  this.render('ShowItem', {data: item});
-});
 
-Router.route('/files/:filename', function () {
-  this.response.end('hi from the server\n');
-}, {where: 'server'});
+// Router.route('/', function () {
+//   this.render('breaking-trail');
+// });
 
-Router.route('/restful', {where: 'server'})
-  .get(function () {
-    this.response.end('get request\n');
-  })
-  .post(function () {
-    this.response.end('post request\n');
-  });
+// Router.route('/activity', function () {
+//   this.render('activity');
+// });
+
+// Router.route('/items/:_id', function () {
+//   var item = Items.findOne({_id: this.params._id});
+//   this.render('ShowItem', {data: item});
+// });
+
+// Router.route('/files/:filename', function () {
+//   this.response.end('hi from the server\n');
+// }, {where: 'server'});
+
+// Router.route('/restful', {where: 'server'})
+//   .get(function () {
+//     this.response.end('get request\n');
+//   })
+//   .post(function () {
+//     this.response.end('post request\n');
+//   });
