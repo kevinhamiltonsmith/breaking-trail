024508d33ce11dff1af9e00bc37ccdf6603c9d9f
Kevin Smith <kesmith@backcountry.com>
2014-12-15 10:43:39 -0700

    set up app structure, added google app keys

---
+++ breaking-trail/server/account.js
@@ -0,0 +1,9 @@
+// first, remove configuration entry in case service is already configured
+ServiceConfiguration.configurations.remove({
+    service: "google"
+});
+ServiceConfiguration.configurations.insert({
+    service: "google",
+    clientId: "1031725881861-r4m8231fh21mm78ukhsoju54p17hn26h.apps.googleusercontent.com",
+    secret: "vCL_EX5W7dTNJkCeXCRn8Prx"
+});
