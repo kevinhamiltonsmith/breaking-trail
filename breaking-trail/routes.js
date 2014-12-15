7383cb28623f2fe4713f74c18e3da924146d658d
Justin Antinarella <jantinarella@backcountry.com>
2014-12-15 14:48:14 -0700

    Start routes

---
+++ breaking-trail/routes.js
@@ -0,0 +1,24 @@
+Router.route('/', function () {
+  this.render('home');
+});
+
+Router.route('/activity', function () {
+  this.render('activity');
+});
+
+Router.route('/items/:_id', function () {
+  var item = Items.findOne({_id: this.params._id});
+  this.render('ShowItem', {data: item});
+});
+
+Router.route('/files/:filename', function () {
+  this.response.end('hi from the server\n');
+}, {where: 'server'});
+
+Router.route('/restful', {where: 'server'})
+  .get(function () {
+    this.response.end('get request\n');
+  })
+  .post(function () {
+    this.response.end('post request\n');
+  });
