24949462bedf468cec06ce1c1aa865e7fa3ca217
Justin Antinarella <jantinarella@backcountry.com>
2014-12-15 15:50:42 -0700

    Smart routes

--- breaking-trail/routes.js
+++ breaking-trail/routes.js
@@ -1,15 +1,45 @@
-Router.route('/', function () {
-  // render the Home template with a custom data context
-  this.render('Home', {data: {title: 'My Title'}});
-});
+// Router.route('/', function () {
+//   // render the Home template with a custom data context
+//   this.render('Home', {data: {title: 'My Title'}});
+// });
 
 // when you navigate to "/home" automatically render the template named "home".
 Router.route('/', function () {
-   this.render('activities');
+  if (Meteor.user()) { 
+    this.render('activities');
+  } else {
+    this.render('loginMessage');
+  }
 });
-Router.route('/home');
-Router.route('/activity');
-Router.route('/activities');
+Router.route('/home', function () {
+  if (Meteor.user()) { 
+    this.render('activities');
+  } else {
+    this.render('loginMessage');
+  }
+});
+Router.route('/activity', function () {
+  if (Meteor.user()) { 
+    this.render('activity');
+  } else {
+    this.render('loginMessage');
+  }
+});
+Router.route('/activities', function () {
+  if (Meteor.user()) { 
+    this.render('activities');
+  } else {
+    this.render('loginMessage');
+  }
+});
+Router.route('/details', function () {
+  if (Meteor.user()) { 
+    this.render('details');
+  } else {
+    this.render('loginMessage');
+  }
+});
+
 
 
 // Router.route('/', function () {
