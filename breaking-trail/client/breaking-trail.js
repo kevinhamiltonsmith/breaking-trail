75aa37de9db5adc1322c21528da9308cdae735f7
Kevin Smith <kesmith@backcountry.com>
2014-12-15 14:09:22 -0700

    added date picker and additional event data

--- breaking-trail/client/breaking-trail.js
+++ breaking-trail/client/breaking-trail.js
@@ -1,16 +1,30 @@
 Template.activities.helpers({
     activities: function() {
         return Events.find({});
+    },
+    activityUsers: function() {
+        return
     }
 });
 
+Template.activity.rendered=function() {
+    $('#activity-date').datepicker();
+};
+
 Template.activity.events({
     'click #create-event-btn': function() {
+        var creator = Meteor.user().profile.name;
+        console.log('creator', creator)
+
         Events.insert({
             activity: $( "#activity-type option:selected" ).text(),
+            date: $('#activity-date').val(),
             details: $("#activity-details").val(),
+            duration: $('#activity-duration').val(),
             name: $("#activity-name").val(),
-            skillLevel: $(".skill-level-btns-wrap input[type=radio]:checked").attr("value")
+            skillLevel: $(".skill-level-btns-wrap input[type=radio]:checked").attr("value"),
+            time: $( "#activity-time option:selected" ).text(),
+            users: [{name: 'kevin'}]
         });
 
         return false;
