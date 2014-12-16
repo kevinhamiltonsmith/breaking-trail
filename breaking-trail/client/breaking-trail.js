d27d92cf24a63c59ff2859b90b71a3c52455c3a4
Justin Antinarella <jantinarella@backcountry.com>
2014-12-16 01:44:26 -0700

    Add users to events.

--- breaking-trail/client/breaking-trail.js
+++ breaking-trail/client/breaking-trail.js
@@ -12,7 +12,7 @@ Template.activity.events({
     'click #create-event-btn': function() {
         var creator = Meteor.user().profile.name;
         var imageLink = Meteor.user().services.google.picture;
-        console.log(creator + " " + imageLink);
+
         Events.insert({
             activity: $( "#activity-type option:selected" ).text(),
             date: $('#activity-date').val(),
@@ -22,9 +22,7 @@ Template.activity.events({
             skillLevel: $(".skill-level-btns-wrap input[type=radio]:checked").attr("value"),
             time: $( "#activity-time option:selected" ).text(),
             creator: {name: creator, image: imageLink},
-            users: [
-                // name: creator, image: imageLink
-            ]
+            users: []
         });
 
         return false;
@@ -33,16 +31,16 @@ Template.activity.events({
 
 Template.activities.events({
     'click #join-event-btn': function() {
-        console.log(this._id);
-        var myName = Meteor.user().profile.name;
-        var myImage = Meteor.user().services.google.picture;
-        var myUsers = this.users;
-        // var existin
-        Events.update({
-            _id: this._id,
-            users: myUsers.push({name: myName, image: myImage})
-        });
-        // get the id 
-        // add to the events collection
+        var name = Meteor.user().profile.name;
+        var imageLink = Meteor.user().services.google.picture;
+        var newUser = this.users;
+        newUser.push({name: name, image: imageLink});
+
+        Events.update(
+            {_id: this._id},
+            { $set:
+                { "users": newUser }
+            }
+        );
     }
 });
