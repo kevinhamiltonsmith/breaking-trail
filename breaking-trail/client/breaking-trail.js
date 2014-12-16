b2431776c0e4100072c6ee0fbeea86bd1289932f
Justin Antinarella <jantinarella@backcountry.com>
2014-12-15 17:56:05 -0700

    Adding to an event

--- breaking-trail/client/breaking-trail.js
+++ breaking-trail/client/breaking-trail.js
@@ -11,7 +11,8 @@ Template.activity.rendered=function() {
 Template.activity.events({
     'click #create-event-btn': function() {
         var creator = Meteor.user().profile.name;
-        //Meteor.user().services.google.picture to get 
+        var imageLink = Meteor.user().services.google.picture;
+        console.log(creator + " " + imageLink);
         Events.insert({
             activity: $( "#activity-type option:selected" ).text(),
             date: $('#activity-date').val(),
@@ -20,18 +21,27 @@ Template.activity.events({
             name: $("#activity-name").val(),
             skillLevel: $(".skill-level-btns-wrap input[type=radio]:checked").attr("value"),
             time: $( "#activity-time option:selected" ).text(),
-            users: [{
-                name: creator
-            }]
+            creator: {name: creator, image: imageLink},
+            users: [
+                // name: creator, image: imageLink
+            ]
         });
 
         return false;
-    },
+    }
+});
 
+Template.activities.events({
     'click #join-event-btn': function() {
-        console.log(this);
-        console.log("im here");
-        console.log(this);
+        console.log(this._id);
+        var myName = Meteor.user().profile.name;
+        var myImage = Meteor.user().services.google.picture;
+        var myUsers = this.users;
+        // var existin
+        Events.update({
+            _id: this._id,
+            users: myUsers.push({name: myName, image: myImage})
+        });
         // get the id 
         // add to the events collection
     }
