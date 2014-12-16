8b9dbda8762cc48bbee96f9bed42d695206cfecc
Kevin Smith <kesmith@backcountry.com>
2014-12-16 14:16:13 -0700

    flash new event on creation

--- breaking-trail/client/breaking-trail.js
+++ breaking-trail/client/breaking-trail.js
@@ -2,9 +2,26 @@ window.BT = {};
 
 Template.activities.helpers({
     activities: function() {
-        return Events.find({}, {sort: [["date", "asc"], ["time", "desc"]]});
+        if (BT.newEvent) {
+            return Events.find({}, {sort: [["createdDate", "desc"], ["date", "asc"]]});
+        } else {
+            return Events.find({}, {sort: [["dateUnix", "asc"], ["time", "desc"]]});
+        }
     }
 });
+
+Template.activities.rendered = function() {
+    if (BT.newEvent) {
+        var $newEvent = $('.single-activity[data-id="' + BT.newEvent + '"]');
+        window.setTimeout(function() {
+            $newEvent.css('background', '#39c');
+        }, 200);
+        window.setTimeout(function() {
+            $newEvent.css('background', '#fff');
+        }, 1200);
+        BT.newEvent = null;
+    }
+};
 
 Template.details.helpers({
     details: function( ) {
@@ -18,18 +35,20 @@ Template.activity.rendered=function() {
 
 Template.activity.events({
     'click #create-event-btn': function() {
-        var creator = Meteor.user().profile.name;
-        var imageLink = Meteor.user().services.google.picture;
+        var creator = Meteor.user().profile.name,
+            imageLink = Meteor.user().services.google.picture,
+            points = Math.floor(Math.random() * (8 - 2)) + 1;
+        
         var insertResult = function(error, id) {
             BT.newEvent = id;
-            BT.sortMethod = "newEvent";
             Router.go('activities');
         };
-        var points = Math.floor(Math.random() * (8 - 2)) + 1;
 
         Events.insert({
             activity: $( "#activity-type option:selected" ).text(),
+            createdDate: Date.now(),
             date: $('#activity-date').val(),
+            dateUnix: new Date($('#activity-date').val()).getTime() / 1000,
             details: $("#activity-details").val(),
             duration: $('#activity-duration').val(),
             name: $("#activity-name").val(),
@@ -41,15 +60,17 @@ Template.activity.events({
             users: [],
             gearItems: [{imageLink: "images/"+points+".jpg", bcLink: "http://www.backcountry.com/"}, {imageLink: "images/"+(points+1)+".jpg", bcLink: "http://www.backcountry.com/"}, {imageLink: "images/"+(points-1)+".jpg", bcLink: "http://www.backcountry.com/"}]
         }, insertResult);
+
         return false;
     }
 });
 
 Template.activities.events({
     'click #join-event-btn': function() {
-        var name = Meteor.user().profile.name;
-        var imageLink = Meteor.user().services.google.picture;
-        var newUser = this.users;
+        var name = Meteor.user().profile.name,
+            imageLink = Meteor.user().services.google.picture,
+            newUser = this.users;
+
         newUser.push({name: name, image: imageLink});
 
         Events.update(
@@ -63,6 +84,6 @@ Template.activities.events({
     'click #show-event-btn': function(e) {
         var temp = $(e.currentTarget).closest(".single-activity").data("id");
         BT.detailsEventId = temp;
-        Router.go('details');    
+        Router.go('details');
     }
 });
